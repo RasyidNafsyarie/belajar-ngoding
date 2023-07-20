300d054eec0f327ae8a528d9b191e21a96a8af55
RasyidNafsyarie <rasyidnafsyarie@gmail.com>
2023-07-20 20:32:46 +0700

    Menyelesaikan Quiz Proxytia

---
+++ Materi/02-ConditionalStatement.js
@@ -0,0 +1,13 @@
+//conditional statement if
+let nilai = 100;
+
+// if (nilai >= 100) 
+//     console.log('Selamat anda lulus')
+
+//if else
+// if (nilai >= 120) {
+//     console.log('Anda lulus dengan predikat cumlaude')
+// } else {
+//     console.log("Anda lulus")
+// }
+
